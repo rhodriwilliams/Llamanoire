98f501482c6cd22d41611728596ddcc40b010ab7
rhun <rh27un@gmail.com>
2019-03-14 13:25:16 +0000

    what?

---
+++ Llama Noire/Assets/_Scripts/EndItAll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndItAll : Interactable {
+
+	
+	void Start () {
+		GetComponent<Rigidbody>().AddForce(0f,1000f,-10000f);
+	}
+	
+	
+}
