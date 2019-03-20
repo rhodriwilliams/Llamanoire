43926dfe5fb104e910bb7b9b677a219684da39dc
rhun <rh27un@gmail.com>
2019-03-20 12:08:04 +0000

    All characters textured

---
+++ Llama Noire/Assets/RandomMat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMat : MonoBehaviour {
+
+	[SerializeField]
+	protected Material[] mats;
+
+	protected SkinnedMeshRenderer _renderer;
+
+	// Use this for initialization
+	void Start () {
+		_renderer = GetComponentInChildren<SkinnedMeshRenderer>();
+		Material[] newMats = new Material[] {mats[Random.Range(0, mats.Length)]};
+		_renderer.materials = newMats;
+	}
+}
