cc7dd13134aaed86bd4eee160ca5455cb3783e42
callum <callumtai@gmail.com>
2019-03-27 15:10:34 +0000

    Added Endgame cutscene camera.

---
+++ Llama Noire/Assets/_Scripts/Environment/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	    void Update ()
+    {
+        transform.Rotate (0,200*Time.deltaTime,0); //rotates 50 degrees per second around z axis
+    }
+}
