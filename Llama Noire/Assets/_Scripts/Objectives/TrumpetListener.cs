ab02ed2f158fa4fefce52b440e0576b5a51fdf4c
callum <callumtai@gmail.com>
2019-03-28 13:33:12 +0000

    Added additional objectives, removed unneeded objectives from objective manager, made sure opening credits weren't still visible during play.

--- Llama Noire/Assets/_Scripts/Objectives/TrumpetListener.cs
+++ Llama Noire/Assets/_Scripts/Objectives/TrumpetListener.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class TrumpetListener : ObjectiveListener {
 	protected override void CompleteObjective(){
+		manager.SetHidden("FindWayOut", false);
 		Destroy(gameObject);
 	}
 
